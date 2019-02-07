3d184ddda71ab311a37ebb1afeba6cd6a0f7f2e7
Malik <deepakm661@gmail.com>
2019-02-07 13:05:37 +0530

    initial setup done

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const app = express();
+const request = require('request')
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended : true}));
+app.set('view engine' , 'hbs')
+
+app.get('/' , (req,res) => {
+  res.render('index' , {answer:null})
+}) 
+
+app.post('/' , (req,res) => {
+ let script = req.body.code;
+ let language = req.body.language;
+ let versionIndex = '0'
+ let clientId = 'a81bbf6be91a5189126fdd4513db6a2e';
+ let clientSecret = 'a8cd1898d259f552bb4fd57ad7a66e17bc689e53801472004e548c4fcf455823';
+ const program = {
+     script : script,
+     language : language,
+     versionIndex : versionIndex,
+     clientId : clientId,
+     clientSecret : clientSecret
+ }
+ request({
+     url: 'https://api.jdoodle.com/v1/execute',
+     method: 'POST',
+     json: program
+ }, (error, response ,body) => {
+      console.log(body)
+      let answer = {
+          output: body.output,
+          memory: body.memory,
+          time:   body.cpuTime
+      }
+      res.render('index' , {answer:answer})
+ })
+ 
+})
+
+
+app.listen(3000 , () => {
+    console.log('server started at port:' + 'http://localhost:3000')
+})
