b83a6fd221fae6b565b53c422cf17f3647a1e685
Malik <deepakm661@gmail.com>
2019-02-07 20:03:22 +0530

    databse saced

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,6 +1,7 @@
 const passport = require('passport')
 const GoogleStrategy = require('passport-google-oauth20')
 const keys = require('./keys')
+const User = require('../models/user-model')
 passport.use(
     new GoogleStrategy({
           callbackURL:'/auth/google/redirect',
@@ -8,5 +9,10 @@ passport.use(
           clientSecret:keys.google.clientSecret
 } , (acessToken,refreshToken,profile,done) => {
            console.log(profile)
+           new User({
+               username:profile.displayName,
+               googleId:profile.id,
+
+           }).save().then((newUser) => {console.log('new user is created:'+ newUser)})
 })
 )
