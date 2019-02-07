92f275ac02ef213ee17fb4b212dcbdbd640bcdb9
Malik <deepakm661@gmail.com>
2019-02-07 21:37:09 +0530

    serialized user

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -2,17 +2,31 @@ const passport = require('passport')
 const GoogleStrategy = require('passport-google-oauth20')
 const keys = require('./keys')
 const User = require('../models/user-model')
+
+passport.serializeUser((user,done) => {
+    done(null,user.id)
+
+})
 passport.use(
     new GoogleStrategy({
           callbackURL:'/auth/google/redirect',
           clientID:keys.google.clientID,
           clientSecret:keys.google.clientSecret
 } , (acessToken,refreshToken,profile,done) => {
-           console.log(profile)
-           new User({
-               username:profile.displayName,
-               googleId:profile.id,
-
-           }).save().then((newUser) => {console.log('new user is created:'+ newUser)})
+           User.findOne({googleId:profile.id}).then((currentUser) => {
+                      if(currentUser) {
+                             console.log('current user is: ' + currentUser)
+                             done(null,currentUser)
+                      }else {
+                        new User({
+                            username:profile.displayName,
+                            googleId:profile.id,
+             
+                        }).save().then((newUser) => {console.log('new user is created:'+ newUser)
+                          done(null,newUser)
+                    })
+                      }
+           })
+          
 })
 )
