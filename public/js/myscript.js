5ba5cd14f50c0ab7cd1fee3df16a3988234f07fa
Malik <deepakm661@gmail.com>
2019-02-11 15:26:33 +0530

    Edit area added

--- public/js/myscript.js
+++ public/js/myscript.js
@@ -1,6 +1,3 @@
-const mongoose = require('mongoose')
-function savedata() {
-   let program = document.getElementsByClassName('user_code')
-   let script = program.value
-   console.log(script)
-}
+$(() => {
+   var editor = CodeMirror
+})
